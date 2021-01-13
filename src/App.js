21e63d214779dc3e7b042217af30c35b20ea4a4c
Tran Bao Phuc <felixtran2000@gmail.com>
2021-01-13 12:40:37 +0700

    fix script tag html

--- src/App.js
+++ src/App.js
@@ -12,27 +12,27 @@ const ACTIONS = {
 
 function App() {
 
-  const [loading, setLoading] = React.useState(false)
+  // const [loading, setLoading] = React.useState(false)
   const updateAction = (value) => {
     console.log(value)
     db.ref('action/')
       .set(value)
       .then(res => console.log(res))
-      .catch(err=> console.log(err))
+      .catch(err => console.log(err))
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Monsterkyle Controller</h2>
-        <div style={{flexDirection:"row", justifyContent:"center"}}>
-        <Button onClick={() => updateAction(1)}>Turn L - 1</Button>
-        <Button onClick={() => updateAction(2)}>Turn R - 2</Button>
-      </div>
-      <Button onClick={() => updateAction(3)}>Action - 3</Button>
-      <Button onClick={() => updateAction(4)}>Action - 4</Button>
+        <div style={{ flexDirection: "row", justifyContent: "center" }}>
+          <Button onClick={() => updateAction(1)}>Turn L - 1</Button>
+          <Button onClick={() => updateAction(2)}>Turn R - 2</Button>
+        </div>
+        <Button onClick={() => updateAction(3)}>Action - 3</Button>
+        <Button onClick={() => updateAction(4)}>Action - 4</Button>
       </header>
-      
+
     </div>
   );
 }
